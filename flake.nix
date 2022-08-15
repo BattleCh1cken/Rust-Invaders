5896a072e44d5f58bb9627172bcf800b082df470
BattleCh1cken <BattleCh1cken@Larkov.de>
2022-08-15 15:34:49 +0200

    initial commit

--- flake.nix
+++ flake.nix
@@ -8,48 +8,47 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, flake-utils, naersk, pre-commit-hooks }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              nixpkgs-fmt.enable = true;
-              # rustfmt.enable = true;
-            };
+  outputs = { self, nixpkgs, flake-utils, naersk, pre-commit-hooks }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      naersk-lib = pkgs.callPackage naersk { };
+    in
+    {
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+            # rustfmt.enable = true;
           };
         };
+      };
 
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = nixpkgs.legacyPackages.${system}.mkShell rec {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            llvmPackages.bintools # To use lld linker
-            cargo
-            rustc
-            gcc
-            rustfmt
-            rust-analyzer
-            clippy
-          ];
-          buildInputs = with pkgs; [
-            udev
-            alsaLib
-            vulkan-loader
-            xlibsWrapper
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi # To use x11 feature
-            libxkbcommon
-            wayland # To use wayland feature
-          ];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        };
-      });
+      defaultPackage = naersk-lib.buildPackage ./.;
+      devShell = nixpkgs.legacyPackages.${system}.mkShell rec {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          llvmPackages.bintools # To use lld linker
+          cargo
+          rustc
+          gcc
+          rustfmt
+          rust-analyzer
+          clippy
+        ];
+        buildInputs = with pkgs; [
+          udev
+          alsaLib
+          vulkan-loader
+          xlibsWrapper
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi # To use x11 feature
+          libxkbcommon
+          wayland # To use wayland feature
+        ];
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+      };
+    });
 }
